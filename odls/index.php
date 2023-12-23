870c487e1c8b2681ae6cb0c0f09b71035b40bb19
salim <salimyaaqoubii@gmail.com>
2023-12-23 20:09:52 +0400

    added

---
+++ odls/index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Diagnostic Lab Services | Home Page</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/modernizr.custom.97074.js"></script>
+<!---- start-smoth-scrolling---->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+ <script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+<!---End-smoth-scrolling---->
+<!--script-->
+<script src="js/jquery.chocolat.js"></script>
+		<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen" charset="utf-8">
+		<!--light-box-files -->
+		<script type="text/javascript" charset="utf-8">
+		$(function() {
+			$('.gallery a').Chocolat();
+		});
+		</script>
+<!--script-->
+
+</head>
+<body>
+	<div class="header" id="home">
+		<div class="header-top">
+			<div class="container">
+			   <div class="logo">
+				  <a href="index.php">ODL<span>Services</span></a>
+				</div>
+				<div class="top-menu">
+					<span class="menu"><img src="images/nav.png" alt=""/> </span>
+						<ul>
+						<li><a href="index.php" class="active scroll">home</a></li>
+						<li><a href="#aboutus" class="scroll">about us</a></li>
+						<li><a href="#gallery" class="scroll">gallery</a></li>
+						<li><a href="admin/login.php">Admin</a></li>
+						<li><a href="user/login.php">Users</a></li>
+						<li><a href="employee/login.php">Employee</a></li>
+					</ul>
+				</div>
+					 <!--script-nav-->
+						 <script>
+						 $("span.menu").click(function(){
+						 $(".top-menu ul").slideToggle("slow" , function(){
+						 });
+						 });
+						 </script>
+		 
+						<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<div class="content">
+			<div class="about-section" id="aboutus">
+				<div class="container">
+					<h3>welcome to the Diagnostic Lab Services !</h3>
+					    <div class="about-grids">
+							<div class="col-md-4 about-grid">
+								<img src="images/CBC Test.jpeg" class="img-responsive" alt="">
+								<h4>CBC Test.</h4>
+								<p>A complete blood count (CBC) is a blood test used to evaluate your overall health and detect a wide range of disorders, including anemia, infection and leukemia.A complete blood count test measures several components and features of your blood, including:
+						Red blood cells, which carry oxygen
+					    White blood cells, which fight infection
+					    Hemoglobin, the oxygen-carrying protein in red blood cells
+					    Hematocrit, the proportion of red blood cells to the fluid component, or plasma, in your blood
+					    Platelets, which help with blood clotting.</p>
+							</div>
+							<div class="col-md-4 about-grid">
+								<img src="images/Hemoglobin Test.jpeg"  class="img-responsive" alt="">
+								<h4>Hemoglobin Test. </h4>
+								<p>A hemoglobin test measures the amount of hemoglobin in your blood. Hemoglobin is a protein in your red blood cells that carries oxygen to your body's organs and tissues and transports carbon dioxide from your organs and tissues back to your lungs. If a hemoglobin test reveals that your hemoglobin level is lower than normal, it means you have a low red blood cell count (anemia). Anemia can have many different causes, including vitamin deficiencies, bleeding and chronic diseases.</p>
+							</div>
+							<div class="col-md-4 about-grid">
+								<img src="images/Blood Glucose Test.jpeg"  class="img-responsive" alt="">
+								<h4>Blood Glucose Test.</h4>
+								<p>A blood glucose test measures the amount of glucose in your blood. Glucose, a type of simple sugar, is your body’s main source of energy. Your body converts the carbohydrates you eat into glucose.
+					Glucose testing is primarily done to check for type 1 diabetes, type 2 diabetes, and gestational diabetes. Diabetes is a condition that causes your blood glucose level to rise.
+					The amount of sugar in your blood is usually controlled by a hormone called insulin. However, if you have diabetes, your body either doesn’t make.</p>
+							</div>
+								<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+		
+	<div class="gallery" id="gallery">
+		<div class="container">
+			<h3>gallery</h3>
+			<div class="gallery-grids">
+			<section>
+				<ul id="da-thumbs" class="da-thumbs">
+					<li>
+						<a href="images/g1.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g1.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g2.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g2.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g3.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g3.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g4.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g4.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>	
+						<a href="images/g5.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g5.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g6.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g6.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g7.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g7.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g8.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g8.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="images/g9.jpg" rel="title" class="b-link-stripe b-animate-go  thickbox">
+							<img src="images/g9.jpg" alt="" />
+							<div>
+								<h5></h5>
+								<span></span>
+							</div>
+						</a>
+					</li>
+					<div class="clearfix"> </div>
+				</ul>
+			</section>
+			<script type="text/javascript" src="js/jquery.hoverdir.js"></script>	
+		<script type="text/javascript">
+			$(function() {
+			
+				$(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+
+			});
+		</script>
+        </div>
+	</div>
+</div>
+<!--//gallery-->
+
+
+	<div class="footer-section">
+						<div class="container">
+							<div class="footer-bottom">
+						<p>Diagnostic Lab Services @ 2023</p>
+									</div>
+							<script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+				</div>
+			</div>
+</body>
+</html>
